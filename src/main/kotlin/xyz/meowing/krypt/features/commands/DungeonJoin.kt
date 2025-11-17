a39c29f90f0d5bd2ce6bff70cc243796cc952810
aurielyn <aurielyn.dev@gmail.com>
2025-11-17 19:12:20 +0530

    Misc: minor changes

--- src/main/kotlin/xyz/meowing/krypt/features/commands/DungeonJoin.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/commands/DungeonJoin.kt
@@ -7,18 +7,22 @@ import xyz.meowing.krypt.Krypt.prefix
 import xyz.meowing.krypt.annotations.Command
 
 @Command
-object JoinDungeonCommand : Commodore("jd") {
+object DungeonJoin : Commodore("jd") {
+    private val regex = Regex("""[^0-9]""")
     private val floorNames = listOf("ENTRANCE", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN")
 
     init {
         runs { floor: GreedyString ->
-            if(floor.string.length > 2) {
+            if (floor.string.length > 2) {
                 KnitChat.fakeMessage("$prefix §fInvalid Floor.")
                 return@runs
             }
 
-            val masterMode = floor.string.contains("m", ignoreCase = true)
-            val floorString = floor.string.replace("e", "0").replace("""[^0-9]""".toRegex(), "")
+            val masterMode = floor.string.contains("m", true)
+            val floorString = floor.string
+                .replace("e", "0", true)
+                .replace(regex, "")
+
             val floorIndex = floorString.toIntOrNull() ?: -1
 
             if (floorIndex < 0 || floorIndex >= floorNames.size) {
@@ -27,7 +31,7 @@ object JoinDungeonCommand : Commodore("jd") {
             }
 
             val dungeonType = (if (masterMode) "MASTER_" else "") + "CATACOMBS"
-            val dungeonString = dungeonType + (if(floorIndex != 0) "_FLOOR_" else "_") + floorNames[floorIndex]
+            val dungeonString = dungeonType + (if (floorIndex != 0) "_FLOOR_" else "_") + floorNames[floorIndex]
 
             KnitChat.sendCommand("joindungeon $dungeonString")
         }
