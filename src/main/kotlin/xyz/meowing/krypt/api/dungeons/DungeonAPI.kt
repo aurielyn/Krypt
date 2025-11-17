47551a5a56de560af5dcdf1807904c4e9e0d3796
David13374 <vasiledavid14@yahoo.com>
2025-11-17 17:28:14 +0200

    refactor: Made secret event for various types of secrets

--- src/main/kotlin/xyz/meowing/krypt/api/dungeons/DungeonAPI.kt
+++ src/main/kotlin/xyz/meowing/krypt/api/dungeons/DungeonAPI.kt
@@ -2,8 +2,16 @@
 
 package xyz.meowing.krypt.api.dungeons
 
+import net.minecraft.client.multiplayer.ClientLevel
+import net.minecraft.core.BlockPos
+import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket
+import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
+import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.entity.monster.Zombie
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.entity.SkullBlockEntity
 import tech.thatgravyboat.skyblockapi.api.data.Perk
 import tech.thatgravyboat.skyblockapi.utils.extentions.getTexture
 import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.find
@@ -11,6 +19,7 @@ import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.findOrNull
 import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.findThenNull
 import tech.thatgravyboat.skyblockapi.utils.regex.matchWhen
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import xyz.meowing.knit.api.KnitClient
 import xyz.meowing.knit.api.KnitPlayer
 import xyz.meowing.krypt.annotations.Module
 import xyz.meowing.krypt.api.dungeons.enums.DungeonClass
@@ -36,13 +45,22 @@ import xyz.meowing.krypt.events.core.ChatEvent
 import xyz.meowing.krypt.events.core.DungeonEvent
 import xyz.meowing.krypt.events.core.EntityEvent
 import xyz.meowing.krypt.events.core.LocationEvent
+import xyz.meowing.krypt.events.core.PacketEvent
 import xyz.meowing.krypt.events.core.TablistEvent
 import xyz.meowing.krypt.events.core.TickEvent
+import xyz.meowing.krypt.utils.Utils.removeFormatting
+import kotlin.collections.first
+import kotlin.collections.isNotEmpty
 import kotlin.math.floor
 
 @Module
 object DungeonAPI {
     private const val MIMIC_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTY3Mjc2NTM1NTU0MCwKICAicHJvZmlsZUlkIiA6ICJhNWVmNzE3YWI0MjA0MTQ4ODlhOTI5ZDA5OTA0MzcwMyIsCiAgInByb2ZpbGVOYW1lIiA6ICJXaW5zdHJlYWtlcnoiLAogICJzaWduYXR1cmVSZXF1aWJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTE5YzEyNTQzYmM3NzkyNjA1ZWY2OGUxZjg3NDlhZThmMmEzODFkOTA4NWQ0ZDRiNzgwYmExMjgyZDM1OTdhMCIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9"
+    private const val RED_SKULL_TEXTURE = "eyJ0aW1lc3RhbXAiOjE1NzA5MTUxODU0ODUsInByb2ZpbGVJZCI6IjVkZTZlMTg0YWY4ZDQ5OGFiYmRlMDU1ZTUwNjUzMzE2IiwicHJvZmlsZU5hbWUiOiJBc3Nhc2luSmlhbmVyMjUiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2EyMjNlMzZhYzEzZjBmNzFhYmNmYmYwYzk2ZmRjMjAxMGNjM2UxMWZmMmIwZDgxMTJkMGU2M2Y0YjRhYWEwZGUifX19"
+    private const val WITHER_ESSENCE_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ==ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ=="
+
+    private val secretTypes = listOf("Architect's First Draft", "Candycomb", "Decoy", "Defuse Kit", "Dungeon Chest Key",
+        "Healing VIII Splash Potion", "Inflatable Jerry", "Revive Stone", "Secret Dye", "Spirit Leap", "Training Weights", "Trap", "Treasure Talisman").sorted()
 
     private val watcherSpawnedAllRegex = Regex("""\[BOSS] The Watcher: That will be enough for now\.""")
     private val watcherKilledAllRegex = Regex("\\[BOSS] The Watcher: You have proven yourself\\. You may pass\\.")
@@ -258,6 +276,59 @@ object DungeonAPI {
 
             mimicKilled = true
         }
+
+        EventBus.registerIn<PacketEvent.Received>(SkyBlockIsland.THE_CATACOMBS) { event ->
+            val packet = event.packet as? ClientboundTakeItemEntityPacket ?: return@registerIn
+
+            val itemId = packet.itemId
+            val world = KnitClient.world
+
+            val entity = world?.getEntity(itemId) as? ItemEntity ?: return@registerIn
+            val name = entity.item.displayName.string.removeFormatting()
+            val sanitizedName = name.drop(1).dropLast(1)
+
+            if(secretTypes.binarySearch(sanitizedName) >= 0)
+                EventBus.post(DungeonEvent.Secrets.Item(itemId))
+        }
+
+        EventBus.registerIn<PacketEvent.Sent>(SkyBlockIsland.THE_CATACOMBS) { event ->
+            val packet = event.packet as? ServerboundUseItemOnPacket ?: return@registerIn
+
+            val pos = packet.hitResult.blockPos ?: return@registerIn
+            val world = KnitClient.world ?: return@registerIn
+            val blockState = world.getBlockState(pos)
+
+            if(blockState.block == Blocks.CHEST || blockState.block == Blocks.TRAPPED_CHEST) {
+                EventBus.post(DungeonEvent.Secrets.Chest(blockState, pos))
+                return@registerIn
+            }
+
+            if(blockState.block == Blocks.LEVER) {
+                EventBus.post(DungeonEvent.Secrets.Misc(DungeonEvent.Secrets.SecretType.LEVER, pos))
+                return@registerIn
+            }
+
+            val texture = getTexture(world, pos)
+
+            if(texture == WITHER_ESSENCE_TEXTURE) {
+                val entity = world.getBlockEntity(pos) ?: return@registerIn
+                EventBus.post(DungeonEvent.Secrets.Essence(entity, pos))
+                return@registerIn
+            }
+
+            if(texture == RED_SKULL_TEXTURE) {
+                val entity = world.getBlockEntity(pos) ?: return@registerIn
+                EventBus.post(DungeonEvent.Secrets.Misc(DungeonEvent.Secrets.SecretType.RED_SKULL, pos))
+                return@registerIn
+            }
+        }
+
+        EventBus.registerIn<EntityEvent.Death>(SkyBlockIsland.THE_CATACOMBS) { event ->
+            if(event.entity.type == EntityType.BAT) {
+                EventBus.post(DungeonEvent.Secrets.Bat(event.entity))
+                return@registerIn
+            }
+        }
     }
 
     fun reset() {
@@ -335,6 +406,23 @@ object DungeonAPI {
     private fun updateHeldItem() {
         val item = KnitPlayer.player?.mainHandItem ?: return
         holdingLeaps = "leap" in item.hoverName.stripped.lowercase()
+    }
+
+    private fun getTexture(world: ClientLevel, pos: BlockPos): String? {
+        val blockEntity = world.getBlockEntity(pos)
+
+        if(blockEntity is SkullBlockEntity) {
+            val profile = blockEntity.ownerProfile ?: return null
+            val texture = profile.properties.get("textures")
+
+            if(texture != null && texture.isNotEmpty()) {
+                return texture.first().value
+            }
+
+            return null
+        }
+
+        return null
     }
 
     // Room accessors
--- src/main/kotlin/xyz/meowing/krypt/features/general/SecretSound.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/general/SecretSound.kt
@@ -1,28 +1,18 @@
 package xyz.meowing.krypt.features.general
 
-import net.minecraft.client.multiplayer.ClientLevel
-import net.minecraft.core.BlockPos
-import net.minecraft.network.protocol.game.ClientboundTakeItemEntityPacket
-import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
 import net.minecraft.sounds.SoundEvent
 import net.minecraft.sounds.SoundEvents
-import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.item.ItemEntity
-import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.entity.SkullBlockEntity
-import tech.thatgravyboat.skyblockapi.platform.properties
 import xyz.meowing.krypt.features.Feature
 import xyz.meowing.krypt.annotations.Module
 import xyz.meowing.krypt.api.location.SkyBlockIsland
 import xyz.meowing.krypt.config.ui.types.ElementType
-import xyz.meowing.krypt.events.core.PacketEvent
 import xyz.meowing.krypt.managers.config.ConfigElement
 import xyz.meowing.krypt.managers.config.ConfigManager
 import xyz.meowing.knit.api.KnitClient
 import xyz.meowing.knit.api.KnitPlayer.player
 import xyz.meowing.krypt.config.ConfigDelegate
-import xyz.meowing.krypt.events.core.EntityEvent
-import xyz.meowing.krypt.utils.Utils.removeFormatting
+import xyz.meowing.krypt.events.core.DungeonEvent
 import kotlin.math.abs
 
 @Module
@@ -66,69 +56,31 @@ object SecretSound : Feature(
             )
     }
 
-
     private val volume by ConfigDelegate<Double>("secretSound.volume")
     private val soundIndex by ConfigDelegate<Int>("secretSound.sound")
     private val pitch by ConfigDelegate<Double>("secretSound.pitch")
 
-    private const val redSkull = "eyJ0aW1lc3RhbXAiOjE1NzA5MTUxODU0ODUsInByb2ZpbGVJZCI6IjVkZTZlMTg0YWY4ZDQ5OGFiYmRlMDU1ZTUwNjUzMzE2IiwicHJvZmlsZU5hbWUiOiJBc3Nhc2luSmlhbmVyMjUiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2EyMjNlMzZhYzEzZjBmNzFhYmNmYmYwYzk2ZmRjMjAxMGNjM2UxMWZmMmIwZDgxMTJkMGU2M2Y0YjRhYWEwZGUifX19"
-    private const val witherEssence = "ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ==ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ=="
-
     private val selectedSound: SoundEvent
         get() = SoundTypes.entries[soundIndex].getSound()
 
-    private val secretTypes = listOf("Architect's First Draft", "Candycomb", "Decoy", "Defuse Kit", "Dungeon Chest Key",
-        "Healing VIII Splash Potion", "Inflatable Jerry", "Revive Stone", "Secret Dye", "Spirit Leap", "Training Weights", "Trap", "Treasure Talisman").sorted()
-
-    private const val secretDistance = 10.0
+    private const val SECRET_DISTANCE = 10.0
 
     override fun initialize() {
-        register<PacketEvent.Sent> { event ->
-            val packet = event.packet as? ServerboundUseItemOnPacket ?: return@register
+        register<DungeonEvent.Secrets.Chest> { playSound() }
+        register<DungeonEvent.Secrets.Misc> { playSound() }
+        register<DungeonEvent.Secrets.Item> { event ->
+            val itemId = event.entityId
 
-            val pos = packet.hitResult.blockPos
-            val world = KnitClient.world ?: return@register
-            val blockState = world.getBlockState(pos)
-
-            if(blockState.block == Blocks.CHEST || blockState.block == Blocks.TRAPPED_CHEST || blockState.block == Blocks.LEVER || checkSkull(world, pos))
-                playSound()
-        }
-        register<PacketEvent.Received> { event ->
-            val packet = event.packet as? ClientboundTakeItemEntityPacket ?: return@register
-
-            val itemId = packet.itemId
             val world = KnitClient.world
-
             val entity = world?.getEntity(itemId) as? ItemEntity ?: return@register
-            val name = entity.item.displayName.string.removeFormatting()
-            val sanitizedName = name.drop(1).dropLast(1)
 
-            if(secretTypes.binarySearch(sanitizedName) >= 0
-                && abs(player!!.position().distanceTo(entity.position())) <= secretDistance)
+            if(abs(player!!.position().distanceTo(entity.position())) <= SECRET_DISTANCE)
                 playSound()
         }
-        register<EntityEvent.Death> { event ->
-            if(event.entity.type == EntityType.BAT
-                && abs(player!!.position().distanceTo(event.entity.position())) <= secretDistance)
+        register<DungeonEvent.Secrets.Bat> { event ->
+            if(abs(player!!.position().distanceTo(event.entity.position())) <= SECRET_DISTANCE)
                 playSound()
         }
-    }
-
-    private fun checkSkull(world: ClientLevel, pos: BlockPos): Boolean {
-        val blockEntity = world.getBlockEntity(pos)
-
-        if(blockEntity is SkullBlockEntity) {
-            val profile = blockEntity.ownerProfile ?: return false
-            val texture = profile.properties.get("textures")
-
-            if(texture != null && texture.isNotEmpty()) {
-                return texture.first().value == witherEssence || texture.first().value == redSkull
-            }
-
-            return false
-        }
-
-        return false
     }
 
     private fun playSound() {
@@ -144,7 +96,9 @@ enum class SoundTypes(private val sound: SoundEvent, private val dropdownName: S
     BLAZE(SoundEvents.BLAZE_HURT, "Blaze"),
     CAT(SoundEvents.CAT_AMBIENT, "Cat"),
     ANVIL(SoundEvents.ANVIL_LAND, "Anvil"),
-    XP(SoundEvents.EXPERIENCE_ORB_PICKUP, "Experience");
+    XP(SoundEvents.EXPERIENCE_ORB_PICKUP, "Experience"),
+    NOTE_BLOCK(SoundEvents.NOTE_BLOCK_PLING.value(), "Note Block")
+    ;
 
     fun getName() = dropdownName
     fun getSound() = sound
