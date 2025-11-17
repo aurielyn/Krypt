477f7dd0639540415ca9816db1626f6909510da1
David13374 <vasiledavid14@yahoo.com>
2025-11-17 18:12:37 +0200

    fix: glhf

--- src/main/kotlin/xyz/meowing/krypt/features/general/SecretSound.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/general/SecretSound.kt
@@ -26,15 +26,12 @@ object SecretSound : Feature(
         BLAZE(SoundEvents.BLAZE_HURT, "Blaze"),
         CAT(SoundEvents.CAT_AMBIENT, "Cat"),
         ANVIL(SoundEvents.ANVIL_LAND, "Anvil"),
-        XP(SoundEvents.EXPERIENCE_ORB_PICKUP, "Experience");
+        XP(SoundEvents.EXPERIENCE_ORB_PICKUP, "Experience"),
+        NOTE_BLOCK(SoundEvents.NOTE_BLOCK_PLING.value(), "Note Block");
 
         fun getName() = dropdownName
         fun getSound() = sound
     }
-
-    private const val secretDistance = 10.0
-    private const val redSkull = "eyJ0aW1lc3RhbXAiOjE1NzA5MTUxODU0ODUsInByb2ZpbGVJZCI6IjVkZTZlMTg0YWY4ZDQ5OGFiYmRlMDU1ZTUwNjUzMzE2IiwicHJvZmlsZU5hbWUiOiJBc3Nhc2luSmlhbmVyMjUiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2EyMjNlMzZhYzEzZjBmNzFhYmNmYmYwYzk2ZmRjMjAxMGNjM2UxMWZmMmIwZDgxMTJkMGU2M2Y0YjRhYWEwZGUifX19"
-    private const val witherEssence = "ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ==ewogICJ0aW1lc3RhbXAiIDogMTYwMzYxMDQ0MzU4MywKICAicHJvZmlsZUlkIiA6ICIzM2ViZDMyYmIzMzk0YWQ5YWM2NzBjOTZjNTQ5YmE3ZSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEYW5ub0JhbmFubm9YRCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lNDllYzdkODJiMTQxNWFjYWUyMDU5Zjc4Y2QxZDE3NTRiOWRlOWIxOGNhNTlmNjA5MDI0YzRhZjg0M2Q0ZDI0IgogICAgfQogIH0KfQ=="
 
     private val dropdownValues = SoundTypes.entries.map { it.getName() }.toList()
 
@@ -44,22 +41,6 @@ object SecretSound : Feature(
 
     private val selectedSound: SoundEvent
         get() = SoundTypes.entries[soundIndex].getSound()
-
-    private val secretTypes = listOf(
-        "Architect's First Draft",
-        "Candycomb",
-        "Decoy",
-        "Defuse Kit",
-        "Dungeon Chest Key",
-        "Healing VIII Splash Potion",
-        "Inflatable Jerry",
-        "Revive Stone",
-        "Secret Dye",
-        "Spirit Leap",
-        "Training Weights",
-        "Trap",
-        "Treasure Talisman"
-    ).sorted()
 
     override fun addConfig() {
         ConfigManager
@@ -95,13 +76,6 @@ object SecretSound : Feature(
             )
     }
 
-    private val volume by ConfigDelegate<Double>("secretSound.volume")
-    private val soundIndex by ConfigDelegate<Int>("secretSound.sound")
-    private val pitch by ConfigDelegate<Double>("secretSound.pitch")
-
-    private val selectedSound: SoundEvent
-        get() = SoundTypes.entries[soundIndex].getSound()
-
     private const val SECRET_DISTANCE = 10.0
 
     override fun initialize() {
@@ -116,12 +90,10 @@ object SecretSound : Feature(
 
             if(abs(player!!.position().distanceTo(entity.position())) <= SECRET_DISTANCE)
                 playSound()
-            }
         }
         register<DungeonEvent.Secrets.Bat> { event ->
             if(abs(player!!.position().distanceTo(event.entity.position())) <= SECRET_DISTANCE)
                 playSound()
-            }
         }
     }
 
@@ -132,16 +104,4 @@ object SecretSound : Feature(
             pitch.toFloat() / 100
         )
     }
-}
-
-enum class SoundTypes(private val sound: SoundEvent, private val dropdownName: String) {
-    BLAZE(SoundEvents.BLAZE_HURT, "Blaze"),
-    CAT(SoundEvents.CAT_AMBIENT, "Cat"),
-    ANVIL(SoundEvents.ANVIL_LAND, "Anvil"),
-    XP(SoundEvents.EXPERIENCE_ORB_PICKUP, "Experience"),
-    NOTE_BLOCK(SoundEvents.NOTE_BLOCK_PLING.value(), "Note Block")
-    ;
-
-    fun getName() = dropdownName
-    fun getSound() = sound
 }
