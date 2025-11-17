26f1189aec87db641a92625ea1b4599b2beb458c
MrFast <mrfastkrunker@gmail.com>
2025-11-16 19:06:05 -0500

    Add Join Dungeon Command, also add shadow to room secrets text

---
+++ src/main/kotlin/xyz/meowing/krypt/features/commands/JoinDungeon.kt
@@ -0,0 +1,35 @@
+package xyz.meowing.krypt.features.commands
+
+import xyz.meowing.knit.api.KnitChat
+import xyz.meowing.knit.api.command.Commodore
+import xyz.meowing.knit.api.command.utils.GreedyString
+import xyz.meowing.krypt.Krypt.prefix
+import xyz.meowing.krypt.annotations.Command
+
+@Command
+object JoinDungeonCommand : Commodore("jd") {
+    private val floorNames = listOf("ENTRANCE", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN")
+
+    init {
+        runs { floor: GreedyString ->
+            if(floor.string.length > 2) {
+                KnitChat.fakeMessage("$prefix §fInvalid Floor.")
+                return@runs
+            }
+
+            val masterMode = floor.string.contains("m", ignoreCase = true)
+            val floorString = floor.string.replace("e", "0").replace("""[^0-9]""".toRegex(), "")
+            val floorIndex = floorString.toIntOrNull() ?: -1
+
+            if (floorIndex < 0 || floorIndex >= floorNames.size) {
+                KnitChat.fakeMessage("$prefix §fInvalid Floor.")
+                return@runs
+            }
+
+            val dungeonType = (if (masterMode) "MASTER_" else "") + "CATACOMBS"
+            val dungeonString = dungeonType + (if(floorIndex != 0) "_FLOOR_" else "_") + floorNames[floorIndex]
+
+            KnitChat.sendCommand("joindungeon $dungeonString")
+        }
+    }
+}
